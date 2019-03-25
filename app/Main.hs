86754c1bdaef6b826cbad32bc94f305ce10581fd
epalle <epalle.cyrille@gmail.com>
2019-03-25 21:16:33 +0100

    minor

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,12 @@
 module Main where
 
 import Brick
-import Brick.Widgets.Center
 import Brick.Widgets.Border
 import Brick.Widgets.Border.Style
+import Brick.Widgets.Center
 
 ui :: Widget ()
-ui =
-    withBorderStyle unicode $
-    borderWithLabel (str "Hello!") $
-    (center (str "Left") <+> vBorder <+> center (str "Right"))
+ui = withBorderStyle unicode $ borderWithLabel (str "Hello!") $ center (str "Left") <+> vBorder <+> center (str "Right")
 
 main :: IO ()
 main = simpleMain ui
